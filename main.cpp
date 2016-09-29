eb58e067d38a60f485d73878602d0b2f07098397
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-29 15:16:04 +0300

    hash_sets coming

--- main.cpp
+++ main.cpp
@@ -14,14 +14,28 @@ struct Node{
 struct List{
     Node *head = nullptr;
     Node *tail = nullptr;
+    int length = 0;
 };
 
-/*Node move_to_pos(List *list, int pos){
-    Node *node = list->head;
+struct hash_Set{
+//    TODO hash sets
+};
+
+bool compare_points(Point point1, Point point2){
+    if (point1.x == point2.x and point1.y == point2.y) return true;
+    else return false;
+}
+
+Node *move_to_pos(List &list, int pos){
+    Node *node = list.head;
+    if (pos == 0) return node;
     for (int i=0; i < pos; i++){
-        node = node->next;
+        if (node != list.tail)
+            node = node->next;
+        else return node;
     }
-}*/
+    return node;
+}
 
 void add_to_list(List &list, Point &point){
     Node *temp = new Node;
@@ -36,6 +50,7 @@ void add_to_list(List &list, Point &point){
         temp->prev = nullptr;
         list.head = list.tail = temp;
     }
+    list.length += 1;
 }
 
 void add_node_before(List &list, Point &point, Node *node){
@@ -53,6 +68,7 @@ void add_node_before(List &list, Point &point, Node *node){
         node->prev->next = add;
         node->prev = add;
     }
+    list.length += 1;
 }
 
 void delete_node(List &list, Node *node){
@@ -67,7 +83,21 @@ void delete_node(List &list, Node *node){
         cerr << "Invalid node!";
         exit(1);
     }
+    list.length -= 1;
 }
+
+int search_node(List &list, Point s_point, bool dir = 1){
+    if (dir){
+        for (int pos = 0; pos < list.length; pos++)
+            if (compare_points(move_to_pos(list, pos)->point, s_point)) return pos;
+    }else {
+        for (int pos = list.length; pos > 0; pos--)
+            if (compare_points(move_to_pos(list, pos)->point, s_point)) return pos;
+    }
+    return 0;
+}
+
+//TODO function 6
 
 void print_point(Point &point){
     cout << "(" << point.x << ", " << point.y << ")" << endl;
@@ -83,15 +113,18 @@ void print_list(List &list){
 
 int main() {
     List list;
-    Point add = {20, 20};
+    Point add = {7, 5};
     for (int i=0; i<6; i++){
         Point a = {rand()%10, rand()%10};
         add_to_list(list, a);
     }
     print_list(list);
     cout << endl;
-    add_node_before(list, add, list.tail);
-    delete_node(list, list.head->next);
+    add_node_before(list, add, move_to_pos(list, 3));
     print_list(list);
+    cout << search_node(list, {7, 5}, 0);
+    //add_node_before(list, add, list.tail);
+    //delete_node(list, list.head->next);
+    //print_list(list);
     return 0;
 }
